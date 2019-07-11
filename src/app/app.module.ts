aab65e1c18461b5939dcd7d607f2387d030b4567
lufeng111 <1836232210@qq.com>
2019-07-12 01:04:39 +0800

    dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductComponent } from './product/product.component';
+import { SearchComponent } from './search/search.component';
+import { StarsComponent } from './stars/stars.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProductComponent,
+    SearchComponent,
+    StarsComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
